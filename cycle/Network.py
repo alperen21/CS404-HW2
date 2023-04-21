a853d81a76f47f77f470c9bab061337419168ab9
alperenyildiz <alperenyildiz@sabanciuniv.edu>
2023-04-21 15:12:01 +0300

    finished

--- cycle/Network.py
+++ cycle/Network.py
@@ -8,26 +8,18 @@ class Graph:
         for x_coordinate, line in enumerate(slantMatrix):
             for y_coordinate, elem in enumerate(line):
                 if elem == 1:
-                   # self.matrix[x_coordinate+1][y_coordinate].children.append(Coordinate(x_coordinate, y_coordinate+1))
                    edgelist.append(
                        (f"({x_coordinate+1},{y_coordinate})", f"({x_coordinate},{y_coordinate+1})")
                    )
 
-                #    edgelist.append(
-                #        (f"({x_coordinate},{y_coordinate+1})", f"({x_coordinate+1},{y_coordinate})")
-                #        )
                 if elem == -1:
                     edgelist.append(
                        (f"({x_coordinate},{y_coordinate})", f"({x_coordinate+1},{y_coordinate+1})")
                    )
-                    # edgelist.append(
-                    #     (f"({x_coordinate+1},{y_coordinate+1})", f"({x_coordinate},{y_coordinate})")
-                    # )
-        
+
         self.network = nx.Graph(edgelist)
 
         return edgelist
     
-    def getCycle(self):
-        for cycle in nx.simple_cycles(self.network):
-            print(cycle)
+    def isCyclic(self):
+        return len(list(nx.simple_cycles(self.network))) > 0
--- main.py
+++ main.py
@@ -2,19 +2,30 @@ from Board.Board import Board, Constraint
 from cycle.Network import Graph
 from pprint import pprint
 
+def getAcylicSolution(solutions):
+    for solution in solutions:
+        graph = Graph()
+        graph.initialize(solution)
+
+        if not graph.isCyclic():
+            return solution
+
+def translate(solution):
+    for x in range(len(solution)):
+        for y in range(len(solution)):
+            if solution[x][y] == 1:
+                solution[x][y] = "/"
+            elif solution[x][y] == -1:
+                solution[x][y] = "\\"
+    
+    return solution
+
 def main():
     board = Board()
     board.initialize("input.txt")
     solution_matrix = board.solve()
-    pprint(solution_matrix[10])
 
-
-    graph = Graph()
-    graph.initialize(solution_matrix[0])
-
-    graph.getCycle()
-    # graph.initialize_children(solution_matrix[10]) 
-    # graph.print()
+    pprint(translate(getAcylicSolution(solution_matrix)))
 
 
 
